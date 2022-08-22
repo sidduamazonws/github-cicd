48853a2bcc0b9fab04369e28456d7ed6955d957f
Siddeswara Reddy <siddeswarareddy@Siddeswaras-MBP.attlocal.net>
2022-08-21 23:39:31 -0500

    first commit

---
+++ src/main/java/com/sample/github/cicd/GithubCicdApplication.java
@@ -0,0 +1,21 @@
+package com.sample.github.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdApplication {
+
+	@GetMapping("/greet")
+	public String welcome() {
+		return "Welcome to github CI/CD ...";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/github/cicd/GithubCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.github.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
